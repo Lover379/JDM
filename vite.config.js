7c01975224308c237489f3111b598f4c9042f096
Maksim Kosykh <ivanivanovich379000@gmail.com>
2026-09-15 11:42:11 +0800

    Add car pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/JDM/"
+  base: "/JDM/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        car: resolve(__dirname, "car.html")
+      }
+    }
+  }
 });
