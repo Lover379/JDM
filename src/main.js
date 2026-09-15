63c61832bffb37cbac55f5c6d8ec47b51647ec6a
Maksim Kosykh <ivanivanovich379000@gmail.com>
2026-09-15 21:24:07 +0800

    version 2.0

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import './style.css';
+
+const currentPage = window.location.pathname.split("/").pop();
+
+document.querySelectorAll(".nav a").forEach(link => {
+  const href = link.getAttribute("href");
+
+  if (!href || href === "#") return;
+
+  const linkPage = href.split("/").pop();
+
+  if (
+    (currentPage === "" || currentPage === "index.html") &&
+    (linkPage === "" || linkPage === "index.html")
+  ) {
+    link.classList.add("active");
+  } else if (linkPage === currentPage) {
+    link.classList.add("active");
+  }
+});
